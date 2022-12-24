f008526f44057889b481878064035aab3d5be0cb
krushabh-dev <rushabhrbs@gmail.com>
2022-12-24 11:21:01 +0530

    g

--- src/Game/Board.jsx
+++ src/Game/Board.jsx
@@ -1,29 +1,29 @@
-import React, {useState} from 'react'
-import Square from './Square'
+import React, { useState } from "react";
+import Square from "./Square";
 
 function Board() {
-const [game, setGame] = useState(Array(9).fill(null));
+  const [game, setGame] = useState(Array(9).fill(null));
   return (
-    <div className='Board'>
-        <div className="board-container">
+    <div className="Board">
+      <div className="board-container">
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-        </div>
-        <div className="board-row">
-        <Square />
-            <Square />
-            <Square />
+          <Square value={game[0]} />
+          <Square value={game[1]} />
+          <Square value={game[2]} />
         </div>
         <div className="board-row">
-        <Square />
-            <Square />
-            <Square />
+          <Square value={game[3]} />
+          <Square value={game[4]} />
+          <Square value={game[5]} />
         </div>
+        <div className="board-row">
+          <Square value={game[6]} />
+          <Square value={game[7]} />
+          <Square value={game[8]} />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Board
+export default Board;
